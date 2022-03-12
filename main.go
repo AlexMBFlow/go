94d691089de7f3c672d12d822f5c632706491477
hit0tsu__ <49010409+hit0tsuu@users.noreply.github.com>
2022-03-13 00:00:31 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	seconds := time.Now().Unix()
+	rand.Seed(seconds)
+	target := rand.Intn(100)
+	fmt.Println("Chosen a random number 1-100")
+
+	reader := bufio.NewReader(os.Stdin)
+	success := false
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("You have", 10-guesses, "guesses left")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if guess < target {
+			fmt.Println("LOW")
+		} else if guess > target {
+			fmt.Println("HIGH")
+		} else {
+			success = true
+			fmt.Println("gussed!")
+			break
+		}
+	}
+
+	if !success {
+		fmt.Println("Sorry, you loose")
+	}
+}
